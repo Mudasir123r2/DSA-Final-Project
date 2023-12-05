2149c8479c1a084a9c9e4c19ae8f38ac609ddd53
Mudasir Mujtaba <115891838+Mudasir123r2@users.noreply.github.com>
2023-12-05 13:54:02 +0500

    Button-Model-Frame-Uploaded

---
+++ ButtonModel.java
@@ -0,0 +1,22 @@
+package Visiulizer;
+
+import javax.swing.DefaultButtonModel;
+
+public class ButtonModel extends DefaultButtonModel    {
+
+    @Override
+    public boolean isPressed() {
+        return false;
+    }
+
+    @Override
+    public boolean isRollover() {
+        return false;
+    }
+
+    @Override
+    public void setRollover(boolean b) {
+        //NOOP
+    }
+
+}
