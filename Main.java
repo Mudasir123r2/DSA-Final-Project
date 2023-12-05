bc6d2b936ddb6e1d82c8e1f8998d24b8ec69ff99
Mudasir Mujtaba <115891838+Mudasir123r2@users.noreply.github.com>
2023-12-05 13:57:54 +0500

    MainClass-Uploaded

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Visiulizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	LoadingPage p = new LoadingPage();
+    	p.loadframe.dispose();
+    	new InputFrame();
+        
+    }
+}
